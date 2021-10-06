64f981c0d48a6ed1c10fb8da012881db63a0d8cf
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-10-06 12:34:20 -0400

    test git

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', "views");
 
+
+app.use('/test', (req,res,next)=>{
+    res.render('test');
+});
+
+
 app.use((req,res,next)=>{
     res.render('index');
 });
+
 app.listen(3000);
