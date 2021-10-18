137775c07fd4eb32b81b81a0e3944b5d4409108a
Ahmed Toumi <ahmed.toumi2002@gmail.com>
2021-10-17 23:13:44 -0400

    verif

--- controllers/platControllers.js
+++ controllers/platControllers.js
@@ -1,27 +1,26 @@
 "use strict";
 var mongoose = require('mongoose');
 
-const plat = require('../model/menu');
+const Plat = require('../model/menu');
 
 // Récupère un article grâce à son id
 //menu.push({name:"riz"})
-let mange = new plat({name:"mange"});
-let bannane = new plat({name:"bannane"});
+// let mange = new plat({name:"mange"});
+// let bannane = new plat({name:"bannane"});
 
-mange.save();
-bannane.save();
-console.log("pattes : ",bannane);
-console.log("riz:", mange  )
+// mange.save();
+// bannane.save();
+// console.log("pattes : ",bannane);
+// console.log("riz:", mange  );
 
 exports.getPlat = (req, res, next) => {
-    console.log('req.params :', req.params.platId);
+    // console.log('req.params :', req.params.platId);
+  //  let id = mongoose.Types.ObjectId(req.params.platId.trim());
+  //   // console.log("ID = ", id);
+  //   const platId = req.params.platId;
+  //   console.log('id  :', platId.length); 
 
-    let id = mongoose.Types.ObjectId(req.params.platId);
-    console.log("ID = ", id);
-
-    // const platId = req.params.platId;
-    // console.log('id  :', platId);
-    plat.findById(id)
+    Plat.findById(req.params.platId)
     .then(plat => {
       res.render('plat', {
         plat: plat,
