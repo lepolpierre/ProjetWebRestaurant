5b7085a901d1906d3a0258d3d8141f6a3cf3e896
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-10-17 22:08:03 -0400

    resolution probleme params route plat

--- controllers/platControllers.js
+++ controllers/platControllers.js
@@ -1,21 +1,27 @@
 "use strict";
-
+var mongoose = require('mongoose');
 
 const plat = require('../model/menu');
 
 // Récupère un article grâce à son id
 //menu.push({name:"riz"})
-// let riz = new plat({name:"riz"});
-// let pattes = new plat({name:"pattes"});
-// riz.save();
-// pattes.save();
-// console.log("PLAT : ",riz);
+let riz = new plat({name:"riz"});
+let pattes = new plat({name:"pattes"});
+
+riz.save();
+pattes.save();
+console.log("pattes : ",pattes);
+console.log("riz:", riz  )
 
 exports.getPlat = (req, res, next) => {
-    console.log('req.params :', req.params);
-    const platId = req.params.platId;
- 
-    plat.findById(platId)
+    console.log('req.params :', req.params.platId);
+
+    let id = mongoose.Types.ObjectId(req.params.platId);
+    console.log("ID = ", id);
+
+    // const platId = req.params.platId;
+    // console.log('id  :', platId);
+    plat.findById(id)
     .then(plat => {
       res.render('plat', {
         plat: plat,
@@ -24,7 +30,7 @@ exports.getPlat = (req, res, next) => {
     })
     .catch(err=>{
       if(err){
-        console.error(err);
+        console.error("ERREUR :",err);
         throw err;
       }
     });
--- routes/unPlat.js
+++ routes/unPlat.js
@@ -6,9 +6,9 @@ const platController = require('../controllers/platControllers');
 
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.end('routes plat');
-});
+// router.get('/', (req,res)=>{
+//     res.end('routes plat');
+// });
 router.get('/:platId', platController.getPlat);
 
 module.exports = router;
