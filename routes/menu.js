45adf66cbc57ac1bb7a02f29c7631b78e42d0bc1
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-11-10 07:14:36 -0500

    menu alternatif / 2 version du menu pour la remise du sprint

--- routes/menu.js
+++ routes/menu.js
@@ -7,8 +7,9 @@ const platControllers = require('../controllers/platControllers');
 
 const router = express.Router();
 
-router.get('/', platControllers.getMenu);
 router.get('/:categorie', platControllers.getMenuCategorie);
-router.get('/2', platControllers.getMenu2);
+router.get('/', platControllers.getMenu);
+//router.get('/2', platControllers.getMenu2);
+
 
 module.exports = router;
