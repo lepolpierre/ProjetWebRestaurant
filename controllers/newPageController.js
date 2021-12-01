d25177b88da377fbfa55c8f4148c1543761babbf
Ahmed Toumi <ahmed.toumi2002@gmail.com>
2021-12-01 10:34:45 -0500

    ajustement

--- controllers/newPageController.js
+++ controllers/newPageController.js
@@ -4,12 +4,24 @@ const Page = require('../models/page');
 const express = require("express");
 
 
+// creation d'une nouvelle page rtf to html
+exports.creatNewPage = (req, res, next) => {
+  res.render('newPage/createPage', {
+    pageTitle: "Création de ta page",
+    user: req.user,
+  });
+};
+
+
+
 //  recuperation de la nouvelle page dans la bd
 exports.getNewpage = (req, res, next) => {
-  console.log("titre",   req.params.titre )
+  console.log("titre",   req.params.titre );
+
   Page.find({titre: req.params.titre })
     .then(page => {
-      console.log({page})
+      console.log({page});
+      
       res.render('newPage/newPage', {
         user: req.user,
         contenu: page[0].contenu,
@@ -25,22 +37,24 @@ exports.getNewpage = (req, res, next) => {
 };
 
 
-// creation d'une nouvelle page rtf to html
-exports.creatNewPage = (req, res, next) => {
-  res.render('newPage/createPage', {
-    user: req.user,
-  });
-};
 
 // ajouter la page a la base de donnees
 exports.addPage = (req, res, next) => {
-  const  titre  = req.body.titre.toString();
+  const  titre  = req.body.titre.toString().replace(/ /g, "-");
   let  contenu  = req.body.pageBody.toString();
-  let search = '"'
+
+  let search = '"';
+  // ajustement "string" de caractères dans la BD 
   let a = contenu.replace(/"/g," ");
   let b = titre.replace(/"/g," ");
-  console.log("contenue",a)
-  let page = new Page({ titre:b, contenu:a })
+  console.log("contenue",a);
+  let page = new Page({ titre:b, contenu:a });
+
+  
   page.save()
+  .then(page => {
+    console.log({page});
+  })
+  .catch(err=>next(err));
     
 };
